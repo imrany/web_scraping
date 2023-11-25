c4bf336a56df57783ca87a3eb49f481ed5ac46de
imrany <imranmat254@gmail.com>
2023-11-25 12:04:13 +0300

    error handling --match

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,21 @@ fn browse_page(url:&str)-> Result<(), Error>{
     let tab=browser.new_tab().unwrap();
 
     // Navigate to url
-    tab.navigate_to(url).unwrap();
+    tab.navigate_to(url)?.wait_until_navigated()?;
     tab.wait_for_element("#userStatusPadding > a").expect("Failed to select element").click().expect("Failed to click");
-    let png_data=tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Png, Some(75),None,true)?;
+    let login_form=tab.wait_for_element("iframe > form > div#loginForm").unwrap();
+    // login_form.wait_for_element("#login_id")?.type_str("WebKit")?;
+    login_form.wait_for_element("#login_password")?;
+    // let png_data=tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Png, Some(75),None,true)?;
+    let png_data=login_form.capture_screenshot(Page::CaptureScreenshotFormatOption::Png)?;
     fs::write("screenshot.png", png_data).unwrap();
 
     Ok(())
 }
 
 fn main() {
-    browse_page("https://www.accreditationnow.com/").ok();
+   match browse_page("https://www.accreditationnow.com/") {
+    Err(e) =>println!("{}", e), 
+    _ => ()
+   }
 }
