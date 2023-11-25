7e0e7583436ec2ec1f8cb88507c211bbf270a086
imrany <imranmat254@gmail.com>
2023-11-25 11:23:35 +0300

    able to take a screenshot on the main page

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
-use reqwest;
+use headless_chrome::{
+    Browser,
+    protocol::cdp::Page,
+};
+use std::fs;
+use anyhow::{Error, Result};
+
+fn browse_page(url:&str)-> Result<(), Error>{
+    let browser=Browser::default().unwrap();
+    let tab=browser.new_tab().unwrap();
+
+    // Navigate to url
+    tab.navigate_to(url).unwrap();
+    tab.wait_for_element("#userStatusPadding > a").expect("Failed to select element").click().expect("Failed to click");
+    let png_data=tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Png, Some(75),None,true)?;
+    fs::write("screenshot.png", png_data).unwrap();
+
+    Ok(())
+}
 
 fn main() {
-    let response=reqwest::blocking::get("https://www.accreditationnow.com/");
-    let data=response.unwrap().text().unwrap();
-    println!("{}", data);
+    browse_page("https://www.accreditationnow.com/").ok();
 }
