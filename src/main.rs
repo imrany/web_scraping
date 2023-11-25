987378287f7aff7f9250b3af0cc156609fee5236
imrany <imranmat254@gmail.com>
2023-11-25 14:24:27 +0300

    find_element method

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ fn browse_page(url:&str)-> Result<(), Error>{
     // Navigate to url
     tab.navigate_to(url)?.wait_until_navigated()?;
     tab.wait_for_element("#userStatusPadding > a").expect("Failed to select element").click().expect("Failed to click");
-    let login_form=tab.wait_for_element("iframe > form > div#loginForm").unwrap();
-    // login_form.wait_for_element("#login_id")?.type_str("WebKit")?;
-    login_form.wait_for_element("#login_password")?;
-    // let png_data=tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Png, Some(75),None,true)?;
-    let png_data=login_form.capture_screenshot(Page::CaptureScreenshotFormatOption::Png)?;
-    fs::write("screenshot.png", png_data).unwrap();
+    let iframe=tab.find_element("iframe")?;
+    println!("{:?}",iframe);
+    // login_form.wait_for_element("#login_password")?;
+    let png_data=tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Png, Some(75),None,true)?;
+    // let png_data=login_form.capture_screenshot(Page::CaptureScreenshotFormatOption::Png)?;
+    fs::write("screenshot.png", png_data)?;
 
     Ok(())
 }
